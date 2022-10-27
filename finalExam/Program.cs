9767a1a3040c5c72621e05c3ad5ebf0f35992d4b
Alexey Chernyshov <lexxfin@me.com>
2022-10-27 14:03:26 +0300

    Task resolved

--- finalExam/Program.cs
+++ finalExam/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("Введите массив строк разной длины, разделяя элементы через пробел: ");
+string[] arr, result;
+arr = Console.ReadLine().Split();
+result = arr.Where(i => i.Length < 4).ToArray();
+Console.WriteLine($"[{String.Join(", ", arr)}] -> [{String.Join(", ", result)}]");
